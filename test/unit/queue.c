5c41f4333eae5fb996eaf2dff78ee343693842f9
Sergey Bronnikov <sergeyb@tarantool.org>
2020-06-18 10:50:37 +0000

    test: make unit test for queue TAP-compliant
    Part of #5000

--- test/unit/queue.c
+++ test/unit/queue.c
@@ -31,7 +31,6 @@
 #include "third_party/queue.h"
 #include "unit.h"
 #include <stdio.h>
-#include <assert.h>
 
 struct elem
 {
@@ -59,11 +58,11 @@ void test0()
 {
 	header();
 	struct elem_queue queue = STAILQ_HEAD_INITIALIZER(queue);
-	printf("Initialized: %s\n", queue2str(&queue));
+	note("Initialized: %s", queue2str(&queue));
 	STAILQ_INIT(&queue);
-	printf("STAILQ_INIT: %s\n", queue2str(&queue));
+	note("STAILQ_INIT: %s", queue2str(&queue));
 	STAILQ_REVERSE(&queue, elem, entry);
-	printf("STAILQ_REVERSE: %s\n", queue2str(&queue));
+	note("STAILQ_REVERSE: %s", queue2str(&queue));
 	footer();
 }
 
@@ -75,9 +74,9 @@ void test1()
 	struct elem_queue queue = STAILQ_HEAD_INITIALIZER(queue);
 	el1.val = 1;
 	STAILQ_INSERT_TAIL(&queue, &el1, entry);
-	printf("STAILQ_INIT: %s\n", queue2str(&queue));
+	note("STAILQ_INIT: %s", queue2str(&queue));
 	STAILQ_REVERSE(&queue, elem, entry);
-	printf("STAILQ_REVERSE: %s\n", queue2str(&queue));
+	note("STAILQ_REVERSE: %s", queue2str(&queue));
 	footer();
 }
 
@@ -91,9 +90,9 @@ void test2()
 	el2.val = 2;
 	STAILQ_INSERT_TAIL(&queue, &el1, entry);
 	STAILQ_INSERT_TAIL(&queue, &el2, entry);
-	printf("STAILQ_INIT: %s\n", queue2str(&queue));
+	note("STAILQ_INIT: %s", queue2str(&queue));
 	STAILQ_REVERSE(&queue, elem, entry);
-	printf("STAILQ_REVERSE: %s\n", queue2str(&queue));
+	note("STAILQ_REVERSE: %s", queue2str(&queue));
 	footer();
 }
 
@@ -108,9 +107,9 @@ void test3()
 	STAILQ_INSERT_TAIL(&queue, &el1, entry);
 	STAILQ_INSERT_TAIL(&queue, &el2, entry);
 	STAILQ_INSERT_TAIL(&queue, &el3, entry);
-	printf("STAILQ_INIT: %s\n", queue2str(&queue));
+	note("STAILQ_INIT: %s", queue2str(&queue));
 	STAILQ_REVERSE(&queue, elem, entry);
-	printf("STAILQ_REVERSE: %s\n", queue2str(&queue));
+	note("STAILQ_REVERSE: %s", queue2str(&queue));
 	footer();
 }
 
@@ -122,44 +121,46 @@ void test_splice()
 	struct elem_queue queue1 = STAILQ_HEAD_INITIALIZER(queue1);
 	struct elem_queue queue2 = STAILQ_HEAD_INITIALIZER(queue2);
 	STAILQ_SPLICE(&queue1, STAILQ_FIRST(&queue1), entry, &queue2);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	STAILQ_SPLICE(&queue2, STAILQ_FIRST(&queue2), entry, &queue1);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	el1.val = 1;
 	el2.val = 2;
 	el3.val = 3;
 	STAILQ_INSERT_TAIL(&queue1, &el1, entry);
 	STAILQ_INSERT_TAIL(&queue1, &el2, entry);
 	STAILQ_INSERT_TAIL(&queue1, &el3, entry);
-	printf("STAILQ_INIT: %s\n", queue2str(&queue1));
+	note("STAILQ_INIT: %s", queue2str(&queue1));
 	STAILQ_SPLICE(&queue1, STAILQ_FIRST(&queue1), entry, &queue2);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	STAILQ_SPLICE(&queue2, STAILQ_FIRST(&queue2), entry, &queue1);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	STAILQ_SPLICE(&queue1, STAILQ_NEXT(STAILQ_FIRST(&queue1), entry),
 					   entry, &queue2);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	STAILQ_SPLICE(&queue2, STAILQ_NEXT(STAILQ_FIRST(&queue2), entry),
 		      entry, &queue1);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	STAILQ_SPLICE(&queue2, STAILQ_FIRST(&queue2), entry, &queue1);
-	printf("q1: %s\n", queue2str(&queue1));
-	printf("q2: %s\n", queue2str(&queue2));
+	note("q1: %s", queue2str(&queue1));
+	note("q2: %s", queue2str(&queue2));
 	footer();
 }
 
 int main()
 {
+	plan(0);
 	test0();
 	test1();
 	test2();
 	test3();
 	test_splice();
+	check_plan();
 	return 0;
 }
