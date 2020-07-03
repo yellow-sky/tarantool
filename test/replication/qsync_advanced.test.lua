5f46c22e915d79a8ccc07dcf1bcc5312c4dfb75f
Sergey Bronnikov <sergeyb@tarantool.org>
2020-07-03 16:19:36 +0000

    [tosquash] replication: advanced test with and wo error injections
    Error injections are not available on release builds and tests that use error
    injections are skipped on release builds. So to skip tests for sync replication
    we should place them in a separate file.
    
    Part of #5055

--- test/replication/qsync_advanced.test.lua
+++ test/replication/qsync_advanced.test.lua
@@ -5,6 +5,7 @@ fiber = require('fiber')
 
 orig_synchro_quorum = box.cfg.replication_synchro_quorum
 orig_synchro_timeout = box.cfg.replication_synchro_timeout
+orig_replication_timeout = box.cfg.replication_timeout
 
 NUM_INSTANCES = 2
 BROKEN_QUORUM = NUM_INSTANCES + 1
@@ -52,29 +53,7 @@ box.space.sync:select{} -- none
 test_run:switch('default')
 box.space.sync:drop()
 
--- Updated replication_synchro_quorum doesn't affect existed tx.
--- Testcase setup.
-test_run:switch('default')
-_ = box.schema.space.create('sync', {is_sync=true, engine=engine})
-_ = box.space.sync:create_index('pk')
--- Testcase body.
-box.cfg{replication_synchro_quorum=BROKEN_QUORUM, replication_synchro_timeout=0.0001}
-box.error.injection.set('ERRINJ_SYNC_TIMEOUT', true)
-test_run:cmd("setopt delimiter ';'")
-_ = fiber.create(function()
-    box.space.sync:insert{1}
-end);
-test_run:cmd("setopt delimiter ''");
-box.cfg{replication_synchro_quorum=NUM_INSTANCES}
-box.error.injection.set('ERRINJ_SYNC_TIMEOUT', false)
-box.space.sync:select{} -- none
-test_run:switch('replica')
-box.space.sync:select{} -- none
--- Testcase cleanup.
-test_run:switch('default')
-box.space.sync:drop()
-
--- [RFC, quorum commit] attempt to write multiple transactions, expected the
+-- [RFC, quorum commit] Attempt to write multiple transactions, expected the
 -- same order as on client in case of achieved quorum.
 -- Testcase setup.
 test_run:switch('default')
@@ -136,6 +115,7 @@ _ = box.space.sync:create_index('pk')
 -- Testcase body.
 box.begin() box.space.sync:insert({1}) box.commit()
 box.begin() box.space.sync:insert({2}) box.commit()
+box.space.sync:select{} -- 1, 2
 -- Testcase cleanup.
 box.space.sync:drop()
 
@@ -169,41 +149,6 @@ box.space.sync:select{} -- 1, 2, 3, 4, 5
 test_run:switch('default')
 box.space.sync:drop()
 
--- [RFC, Synchronous replication enabling] "As soon as last operation of
--- synchronous transaction appeared in leader's WAL, it will cause all
--- following transactions - no matter if they are synchronous or not - wait for
--- the quorum. In case quorum is not achieved the 'rollback' operation will
--- cause rollback of all transactions after the synchronous one."
--- Testcase setup.
-test_run:switch('default')
-box.cfg{replication_synchro_quorum=NUM_INSTANCES, replication_synchro_timeout=0.1}
-_ = box.schema.space.create('sync', {is_sync=true, engine=engine})
-_ = box.space.sync:create_index('pk')
--- Testcase body.
-box.space.sync:insert{1}
-box.space.sync:select{} -- 1
-test_run:switch('replica')
-box.space.sync:select{} -- 1
-test_run:switch('default')
-box.cfg{replication_synchro_quorum=BROKEN_QUORUM, replication_synchro_timeout=0.1}
-box.error.injection.set('ERRINJ_SYNC_TIMEOUT', true)
-test_run:cmd("setopt delimiter ';'")
-_ = fiber.create(function()
-    box.space.sync:insert{2}
-end);
-test_run:cmd("setopt delimiter ''");
--- Disable synchronous mode.
-disable_sync_mode()
--- Space is in async mode now.
-box.space.sync:insert{3} -- async operation must wait sync one
-box.error.injection.set('ERRINJ_SYNC_TIMEOUT', false)
-box.space.sync:select{} -- 1
-test_run:cmd('switch replica')
-box.space.sync:select{} -- 1
--- Testcase cleanup.
-test_run:switch('default')
-box.space.sync:drop()
-
 -- Warn user when setting `replication_synchro_quorum` to a value
 -- greater than number of instances in a cluster, see gh-5122.
 box.cfg{replication_synchro_quorum=BROKEN_QUORUM} -- warning
@@ -233,47 +178,6 @@ test_run:switch('default')
 box.cfg{read_only=false}
 test_run:switch('replica')
 box.cfg{read_only=true}
--- Testcase cleanup.
-test_run:switch('default')
-box.space.sync:drop()
-
--- Check behaviour with failed write to WAL on master (ERRINJ_WAL_IO).
--- Testcase setup.
-test_run:switch('default')
-box.cfg{replication_synchro_quorum=NUM_INSTANCES, replication_synchro_timeout=0.1}
-_ = box.schema.space.create('sync', {is_sync=true, engine=engine})
-_ = box.space.sync:create_index('pk')
--- Testcase body.
-box.space.sync:insert{1}
-box.space.sync:select{} -- 1
-box.error.injection.set('ERRINJ_WAL_IO', true)
-box.space.sync:insert{2}
-box.error.injection.set('ERRINJ_WAL_IO', false)
-box.space.sync:select{} -- 1
-test_run:switch('replica')
-box.space.sync:select{} -- 1
--- Testcase cleanup.
-test_run:switch('default')
-box.space.sync:drop()
-
--- [RFC, quorum commit] check behaviour with failure answer from a replica
--- (ERRINJ_WAL_SYNC) during write, expected disconnect from the replication
--- (gh-5123, set replication_synchro_quorum to 1).
--- Testcase setup.
-test_run:switch('default')
-box.cfg{replication_synchro_quorum=2, replication_synchro_timeout=0.1}
-_ = box.schema.space.create('sync', {is_sync=true, engine=engine})
-_ = box.space.sync:create_index('pk')
--- Testcase body.
-box.space.sync:insert{1}
-box.space.sync:select{} -- 1
-test_run:switch('replica')
-box.error.injection.set('ERRINJ_WAL_IO', true)
-test_run:switch('default')
-box.space.sync:insert{2}
-test_run:switch('replica')
-box.error.injection.set('ERRINJ_WAL_IO', false)
-box.space.sync:select{} -- 1
 -- Testcase cleanup.
 test_run:switch('default')
 box.space.sync:drop()
