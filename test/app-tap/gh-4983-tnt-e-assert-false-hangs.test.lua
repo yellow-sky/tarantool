20f64588aedf55188d368a789959d66bb04a0bc7
Artem Starshov <artemreyt@tarantool.org>
2020-12-08 16:37:59 +0300

    lua: fix running init lua script
    When tarantool launched with -e flag and in
    script after there is an error, program hangs.
    This happens because shed fiber launches separate
    fiber for init user script and starts auxiliary
    event loop. It's supposed that fiber will stop
    this loop, but in case of error in script, fiber
    tries to stop a loop when the last one isn't
    started yet.
    
    Added a flag, which will watch is loop started and
    when fiber tries to call `ev_break()` we can be sure
    that loop is running already.
    
    Fixes #4983

---
+++ test/app-tap/gh-4983-tnt-e-assert-false-hangs.test.lua
@@ -0,0 +1,46 @@
+#!/usr/bin/env tarantool
+
+local common = require('process_timeout')
+local tap = require('tap')
+local fio = require('fio')
+
+--
+-- gh-4983: tarantool -e 'assert(false)' hangs
+--
+
+local TARANTOOL_PATH = arg[-1]
+local output_file = fio.abspath('out.txt')
+local line = ('%s -e "assert(false)" > %s 2>&1 & echo $!'):
+        format(TARANTOOL_PATH, output_file)
+
+-- Like a default timeout for `cond_wait` in test-run
+local process_waiting_timeout = 60.0
+local file_read_timeout = 60.0
+local file_open_timeout = 60.0
+local file_read_interval = 0.2
+
+local res = tap.test('gh-4983-tnt-e-assert-false-hangs', function(test)
+    test:plan(2)
+
+    local pid = tonumber(io.popen(line):read('*line'))
+    assert(pid, 'pid of proccess can\'t be recieved')
+
+    local process_completed = common.wait_process_completion(pid,
+            process_waiting_timeout)
+
+    test:ok(process_completed,
+            ('tarantool process with pid = %d completed'):format(pid))
+
+    -- Kill process if hangs.
+    if not process_completed then ffi.C.kill(pid, 9) end
+
+    local fh = common.open_with_timeout(output_file, file_open_timeout)
+    assert(fh, 'error while opening ' .. output_file)
+
+    local data = common.read_with_timeout(fh, file_read_timeout, file_read_interval)
+    test:like(data, 'assertion failed', 'assertion failure is displayed')
+
+    fh:close()
+end)
+
+os.exit(res and 0 or 1)
