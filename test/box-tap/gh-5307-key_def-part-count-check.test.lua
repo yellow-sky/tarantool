1054a80351075eee797fb9b85b1201e5cab59400
Sergey Nikiforov <void@tarantool.org>
2020-12-21 15:02:27 +0300

    lua/key_def: fix compare_with_key() part count check
    Added corresponding test
    
    Fixes: #5307

---
+++ test/box-tap/gh-5307-key_def-part-count-check.test.lua
@@ -0,0 +1,29 @@
+#!/usr/bin/env tarantool
+
+local tap = require('tap')
+local mytest = tap.test('key_def part count tests')
+
+mytest:plan(3)
+
+local key_def = require('key_def')
+local kd = key_def.new({{fieldno = 1, type = 'unsigned'}})
+local ok, res
+
+-- Should succeed
+ok, res = pcall(kd.compare_with_key, kd, {1}, {1})
+print("First: ", ok, res)
+mytest:ok(ok and res == 0, "Simple equality")
+
+-- Should succeed
+ok, res = pcall(kd.compare_with_key, kd, {1}, {2})
+print("Second: ", ok, res)
+mytest:ok(ok and res < 0, "Simple inequality")
+
+-- Should fail
+local exp_err = "Invalid key part count (expected [0..1], got 9)"
+ok, res = pcall(kd.compare_with_key, kd, {1}, {1, 2, 3, 4, 5, 6, 7, 8, 9})
+print("Third: ", ok, res)
+mytest:is_deeply({ok, tostring(res)}, {false, exp_err},
+    "Invalid key part count")
+
+os.exit(mytest:check() and 0 or 1)
