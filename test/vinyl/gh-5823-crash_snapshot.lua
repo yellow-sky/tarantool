0885b67a56f000c968f0b5f9f485d314bdbaa73a
Nikita Pettik <korablev@tarantool.org>
2021-02-25 02:26:51 +0200

    vinyl: skip vylog if it's newer than snap
    Having data in different engines checkpoint process is handled this way:
     - wait_checkpoint memtx
     - wait_checkpoint vinyl
     - commit_checkpoint memtx
     - commit_checkpoint vinyl
    
    In contrast to commit_checkpoint which does not tolerate fails (if
    something goes wrong e.g. renaming of snapshot file - instance simply
    crashes), wait_checkpoint may fail. As a part of wait_checkpoint for
    vinyl engine vy_log rotation takes place: old vy_log is closed and new
    one is created. At this moment, wait_checkpoint of memtx engine has
    already created new *inprogress* snapshot featuring bumped vclock.
    While recovering from this configuration, vclock of the latest snapshot
    is used as a reference.
    
    At the initial recovery stage (vinyl_engine_begin_initial_recovery),
    we check that snapshot's vclock matches with vylog's one (they should be
    the same since normally vylog is rotated along with snapshot). On the
    other hand, in the directory we have old snapshot and new vylog (and new
    .inprogress snapshot). In such a situation recovery (even in force mode)
    was aborted. The only way to fix this dead end, user has to manually
    delete last vy_log file.
    
    Let's proceed with the same resolution while user runs force_recovery
    mode: delete last vy_log file and update vclock value. If user uses
    casual recovery, let's print verbose message how to fix this situation
    manually.
    
    Closes #5823

---
+++ test/vinyl/gh-5823-crash_snapshot.lua
@@ -0,0 +1,27 @@
+#!/usr/bin/env tarantool
+
+--
+-- mode == 0: casual bootstrap;
+-- mode == 1: force recovery bootstrap;
+-- mode == 2: casual bootstrap and fill in data.
+--
+local mode = tonumber(arg[1])
+box.cfg ({
+    replication = os.getenv("MASTER"),
+    vinyl_memory = 128 * 1024 * 1024,
+    force_recovery = (mode == 1),
+})
+
+if mode == 2 then
+    local v = box.schema.space.create('test_v', {engine = 'vinyl'})
+    v:create_index('pk', {run_count_per_level = 100, page_size = 128, range_size = 1024})
+    local m = box.schema.space.create('test_m')
+    m:create_index('pk')
+    local str = string.rep('!', 100)
+    for i = 1,10 do v:insert{i, str} end
+    for i = 1,10 do m:insert{i, str} end
+    box.error.injection.set("ERRINJ_SNAP_COMMIT_FAIL", true);
+    box.snapshot()
+end
+
+require('console').listen(os.getenv('ADMIN'))
